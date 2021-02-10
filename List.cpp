ecf5319b54cb9353db061b9a9802cc888a70ddf9
Maniacswimmer <sanderpall@hotmail.com>
2021-02-10 18:46:18 +0000

    Added most of the stuff to the List class.

--- List.cpp
+++ List.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "List.h"
+#include <iostream>
 
 class List
 {
@@ -12,8 +13,28 @@ private:
 	int size = 0;
 	int capacity = 0;
 
+	// Write a word to some memory.
+	void write(char* old, char* new_)
+	{
+		for (auto i = 0; i < size; i++)
+		{
+			new_[i] = old[i];
+		}
+	}
+
+	// Resize the container.
+	void resize()
+	{
+		// Todo: find out how to do the pointer switching.
+		// new_container[capacity * 2];
+		// write from container to new_container.
+		// delete container.
+		// point to new_container.
+	}
+
 public:
 
+	// Initialize an instance of List.
 	explicit List(int size)
 	{
 		container = new char[size];
@@ -21,19 +42,26 @@ public:
 		capacity = size;
 	}
 
-	void set()
+	// Set value at index.
+	void set(int index, char value)
 	{
-
+		// Todo: Encapsulate in a try/catch clause.
+		container[index] = value;
 	}
 
-	char get()
+	// Get value at index.
+	char get(int index)
 	{
-		return 'a';
+		// Todo: Encapsulate in a try/catch clause.
+		return container[index];
 	}
 
-	void append()
+	// Append a value to the end of the List.
+	void append(char value)
 	{
-
+		// Todo: Encapsulate in a try/catch clause.
+		container[size + 1] = value;
+		size++;
 	}
 
 };
