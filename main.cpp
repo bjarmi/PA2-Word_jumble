16e08afb75ca5251797732bbdd56d98f86ed14d7
bjarmi <bjarmiae@gmail.com>
2021-02-11 14:45:47 +0000

    Added fetch_word().

--- main.cpp
+++ main.cpp
@@ -2,16 +2,52 @@
 #include <array>
 #include "List.h"
 #include "Scramble.h"
+#include <fstream>
+#include <cstdlib>
 
 
 struct WordBank
 {
 	static char wordbank[13];
+    static int& word_count;
 
-	// Todo: Fetch word from a wordbank.
+    static int count_words()
+    {
+        std::ifstream file;
+        file.open(wordbank, std::ios::in);
+        if (file.is_open())
+        {
+            return std::count(
+                    std::istreambuf_iterator<char>(file),
+                    std::istreambuf_iterator<char>(), '\n'
+            );
+        }
+
+        return -1;
+    }
+
 	static List fetch_word()
 	{
+        int line_number = std::rand() % word_count + 1;
 
+        std::ifstream file(wordbank, std::ios::in);
+
+        std::string line;
+        int count = 0;
+        while (std::getline(file, line))
+        {
+            if (count == line_number)
+            {
+                List word_list;
+
+                for(char i : line)
+                    word_list.append(i);
+
+                return word_list;
+            }
+            count++;
+        }
+        file.close();
 	}
 };
 
